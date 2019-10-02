6ea3d42ecabb3f0522eafdac6062b09cef7a8822
claire demont <claire.edith.demont@gmail.com>
2019-10-02 16:32:53 +0200

    update bundler"

--- app/javascript/components/index.js
+++ app/javascript/components/index.js
@@ -1,23 +1,24 @@
-  const displayByCategory = (category, results) => {
-    const filteredResults = results.filter(result => result.dataset.category == category);
-    const ulExperiences = document.querySelector(".results");
-    ulExperiences.innerHTML = "";
-    filteredResults.forEach((result) => {
-      console.log(result);
-      ulExperiences.insertAdjacentHTML("beforeend", result.innerHTML);
-    });
-  };
+  // const displayByCategory = (category, results) => {
+  //   const filteredResults = results.filter(result => result.dataset.category == category);
+  //   const ulExperiences = document.querySelector(".results");
+  //   ulExperiences.innerHTML = "";
+  //   filteredResults.forEach((result) => {
+  //     console.log(result);
+  //     ulExperiences.insertAdjacentHTML("beforeend", result.innerHTML);
+  //   });
+  // };
 
-  const findCategory = () => {
-    const categories = document.querySelectorAll(".tab-category");
-    const results = Array.from(document.querySelectorAll('.results-card'));
-    categories.forEach((category) => {
-      category.addEventListener("click", event => {
-      const selectedCategory = category.innerText;
-      displayByCategory(selectedCategory, results);
-      });
-    });
-  };
+//   const findCategory = () => {
+//     const categories = document.querySelectorAll(".tab-category");
+//     const results = Array.from(document.querySelectorAll('.results-card'));
+//     console.log(results);
+//     categories.forEach((category) => {
+//       category.addEventListener("click", event => {
+//       const selectedCategory = category.innerText;
+//       displayByCategory(selectedCategory, results);
+//       });
+//     });
+//   };
 
-export { findCategory };
+// export { findCategory };
 
