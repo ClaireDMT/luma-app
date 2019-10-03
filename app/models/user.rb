50a2548138b371b6a06940892d0433a5c4dfe576
claire demont <claire.edith.demont@gmail.com>
2019-10-03 17:37:12 +0200

    remove validation from company and change devise french trad

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :orders
   has_many :posts, dependent: :destroy
   has_many :experiences, through: :orders
-  belongs_to :company
+  # belongs_to :company
   validates :first_name, :last_name, presence: true
 
   # after_create :send_welcome_email
